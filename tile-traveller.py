743fa38271615c0ffdf8da7e1df76db08def0d4e
magnimarels <70704459+magnimarels@users.noreply.github.com>
2020-09-17 16:07:41 +0000

    Finished code

--- tile-traveller.py
+++ tile-traveller.py
@@ -1,147 +1,89 @@
-# Fyrst ætlum við að búa til fall sem tekur inn bool: boolN, boolS, boolW og boolE.
-# Svo búum við if statemetn fyrir hvert tile. Það gefur svo út hvað á að prenta
-# 
+
 player_location = "1, 1"
 
-def what_direction(N, E, S, W):
-    output = ""
-    if N == True:
-        output = output + "(N)orth "
-    if E == True:
-        output = output + "(E)ast"
-    if S == True:
-        output = output + "(S)outh"
-    if W == True:
-        output = output + "(W)est"
 
-if player_location == "1, 1":
-    to_be_printed = what_direction(True, False, False, False)
-    print("You can travel: {} or {}.".format(to_be_printed))
-    player_input = input("Direction: ").capitalize()
-    if player_input == "N":
-        player_location = "1, 2"
-    else:
-        print("Not a valid direction!")
-        print("You can travel: {} or {}.".format(to_be_printed))
-if player_location == "1, 2":
-    to_be_printed = what_direction(True, True, True, False)
-    print("You can travel: {} or {} or {}.".format(to_be_printed))
-    player_input = input("Direction: ").capitalize()
-    if player_input == "N":
-        player_location = "1, 2"
-    elif player_input == "S":
-        player_location = "2, 2"
-    elif player_input == "W":
-        player_location = "1, 3"
-    else:
-        print("Not a valid direction!")
-        print("You can travel: {} or {} or {}.".format(to_be_printed))
-if player_location == "1, 3":
-    to_be_printed = what_direction(False, True, True, False)
-    print("You can travel: {} or {}.".format(to_be_printed))
-    player_input = input("Direction: ").capitalize()
-    if player_input == "E":
-        player_location = "2,3"
-    elif player_input == "S":
-        player_location = "1, 2"
-    else:
-        print("Not a valid direction!")
-        print("You can travel: {} or {}.".format(to_be_printed))
-        
+while player_location != "3, 1":
+    if player_location == "1, 1":
 
+        print("You can travel: (N)orth.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "N":
+            player_location = "1, 2"
+        else:
+            print("Not a valid direction!")
+    if player_location == "1, 2":
 
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "N":
+            player_location = "1, 3"
+        elif player_input == "S":
+            player_location = "1, 1"
+        elif player_input == "E":
+            player_location = "2, 2"
+        else:
+            print("Not a valid direction!")
+    if player_location == "1, 3":
 
+        print("You can travel: (E)ast or (S)outh.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "E":
+            player_location = "2, 3"
+        elif player_input == "S":
+            player_location = "1, 2"
+        else:
+            print("Not a valid direction!")
+    if player_location == "2, 1":
 
+        print("You can travel: (N)orth.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "N":
+            player_location = "2, 2"
+        else:
+            print("Not a valid direction!")
 
+    if player_location == "2, 2":
 
+        print("You can travel: (S)outh or (W)est.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "S":
+            player_location = "2, 1"
+        elif player_input == "W":
+            player_location = "1, 2"
+        else:
+            print("Not a valid direction!")
 
+    if player_location == "2, 3":
 
+        print("You can travel: (E)ast or (W)est.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "E":
+            player_location = "3, 3"
+        elif player_input == "W":
+            player_location == "1, 3"
+        else:
+            print("Not a valid direction!")
 
+    if player_location == "3, 3":
 
+        print("You can travel: (S)outh or (W)est.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "W":
+            player_location = "2, 3"
+        elif player_input == "S":
+            player_location = "3, 2"
+        else:
+            print("Not a valid direction!")
 
+    if player_location == "3, 2":
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if player_location == "2, 1":
-    to_be_printed = what_direction(True, False, False, False)
-    print("You can travel: ", to_be_printed, "." )
-    player_input = input("Direction: ").capitalize()
-    if player_input == "N":
-        player_location = "2, 2"
-    else:
-        print("Not a valid direction!")
-
-if player_location == "2, 2":
-    to_be_printed = what_direction(False, False, True, True)
-    print("You can travel: ", to_be_printed, "." )
-    player_input = input("Direction: ").capitalize()
-    if player_input == "S":
-        player_location = "2, 1"
-    elif player_input == "W":
-        player_location = "1, 2"
-    else:
-        print("Not a valid direction!")
-
-if player_location == "2, 3":
-    to_be_printed = what_direction(False, True, False, True)
-    print("You can travel: ", to_be_printed, "." )
-    player_input = input("Direction: ").capitalize()
-    if player_input == "E":
-        player_location = "3, 3"
-    elif player_input == "W":
-        player_location == "1, 3"
-
-
-
-
-
-
-
-
-
-
-
-
-
-if player_location == "3, 3":
-    to_be_printed = what_direction(False, False, True, True)
-    print("You can travel: ", to_be_printed, "." )
-    player_input = input("Direction: ").capitalize()
-    if player_input == "E":
-        player_location = "2, 3"
-    elif player_input == "S":
-        player_location = "3, 2"
-    else:
-        print("Not a valid direction!")
-
-if player_location == "3, 2":
-    to_be_printed = what_direction(True, False, True, False)
-    print("You can travel: ", to_be_printed, "." )
-    player_input = input("Direction: ").capitalize()
-    if player_input == "N":
-        player_location = "3, 3"
-    elif player_input == "S":
-        print("Victory!")
-        break
-
-    else:
-        print("Not a valid direction!")
+        print("You can travel: (N)orth or (S)outh.")
+        player_input = input("Direction: ").capitalize()
+        if player_input == "N":
+            player_location = "3, 3"
+        elif player_input == "S":
+            player_location = "3, 1"
+        else:
+            print("Not a valid direction!")
+else: 
+    print("Victory!")
