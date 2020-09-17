a412423ef314dd9d8f46112ac967977bafdb389b
olafure20 <70910727+olafure20@users.noreply.github.com>
2020-09-17 15:02:02 +0000

    Skrifa kóða fyrir 2,1 til 2,3

--- tile-traveller.py
+++ tile-traveller.py
@@ -41,4 +41,40 @@ if player_location == "1, 2":
 
 
 
-          
+
+
+
+
+
+
+
+if player_location == "2, 1":
+    to_be_printed = what_direction(True, False, False, False)
+    print("You can travel: ", to_be_printed, "." )
+    player_input = input("Direction: ").capitalize()
+    if player_input == "N":
+        player_location = "2, 2"
+    else:
+        print("Not a valid direction!")
+
+if player_location == "2, 2":
+    to_be_printed = what_direction(False, False, True, True)
+    print("You can travel: ", to_be_printed, "." )
+    player_input = input("Direction: ").capitalize()
+    if player_input == "S":
+        player_location = "2, 1"
+    elif player_input == "W":
+        player_location = "1, 2"
+    else:
+        print("Not a valid direction!")
+
+if player_location == "2, 3":
+    to_be_printed = what_direction(False, True, False, True)
+    print("You can travel: ", to_be_printed, "." )
+    player_input = input("Direction: ").capitalize()
+    if player_input == "E":
+        player_location = "3, 3"
+    elif player_input == "W":
+        player_location == "1, 3"
+    else:
+        print("Not a valid direction!")
