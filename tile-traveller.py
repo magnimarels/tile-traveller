b714646ca9e6ec1c7041de88ed6db451066b8070
rflamegames <40735780+rflamegames@users.noreply.github.com>
2020-09-17 14:29:09 +0000

    start of algoritm and code

--- tile-traveller.py
+++ tile-traveller.py
@@ -1,3 +1,23 @@
-print("hello")
+# Fyrst ætlum við að búa til fall sem tekur inn bool: boolN, boolS, boolW og boolE.
+# Svo búum við if statemetn fyrir hvert tile. Það gefur svo út hvað á að prenta
+# 
+player_location = "1, 1"
 
-print("CHELLO")
+def what_direction(N, S, A, W):
+    output = ""
+    if N == True:
+        output = output +   "North "
+
+
+if player_location == "1, 1":
+    to_be_printed = what_direction(True, False, False, False)
+    print("You can travel: ", to_be_printed, "." )
+    player_input = input("Direction: ")
+    if player_location
+
+
+
+
+
+
+          
